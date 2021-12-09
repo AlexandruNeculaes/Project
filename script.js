07d967fd7a82be5b7a7fdca85809ca80c1d4cdfe
Daniel Hahn <danhahn15@gmail.com>
2021-12-09 13:36:35 +0000

    cart javascript

--- script.js
+++ script.js
@@ -13,3 +13,122 @@ function myFunction() {
     x.style.display = "none";
 
 }
+
+// Cart JS - Daniel Hahn 
+let carts = document.querySelectorAll('.btn btn-danger');
+
+//shop page
+let products = [
+    {
+        name: 'Babolat Pure Strike',
+        tag: '',
+        price: 100.00,
+        inCart: 0
+    }
+]
+
+//sale page
+
+//new page
+
+for(let i=0; i<carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if(productNumbers){
+        document.querySelector('.cart span').textContent=productNumbers;
+    }
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers+1);
+        document.querySelector('.cart span').textContent=productNumbers+1;
+    }
+    else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent=1;
+    }
+    setItems(product);
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems= {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart +=1;
+    }
+    else{
+        product.inCart=1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify
+    (cartItems));
+}
+
+function totalCost(product){
+    let cartCost=localStorage.getItem('totalCost');
+    if(cartCost != null){
+        cartCost=parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }
+    else{
+        localStorage.setItem("totalCost", product.price);
+    }
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem('productsInCart')
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost=localStorage.getItem('totalCost');
+    if(cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="./images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price">€${item.price}</div>
+            <div class="quantity">
+                <ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class=increase" name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div class="total">
+                €${item.inCart * item.price}
+            </div>
+            `;
+        });
+
+        product.container.innerHTML += `
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+                Basket Total
+            </h4>
+            <h4 class="basketTotal">
+                €${cartCost}
+            </h4>
+        `;
+
+    }
+}
+
+onLoadCartNumbers();
