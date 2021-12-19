7b8722cb8b0d5e03812ca18a3d2b1fc8f5a7a8e3
AlexandruNeculaes <alexandru.neculaes@gmail.com>
2021-12-19 16:05:01 +0000

    lastest check

---
+++ script/form.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    event.preventDefault();
+    var fname = document.getElementById("fname").value;
+    var mail = document.getElementById("email").value;
+    alert(fname+", thank you for your details. We will be in touch via "+mail+" shortly.");
+    var x=document.getElementById("form");
+    x.style.display = "none";
+}
